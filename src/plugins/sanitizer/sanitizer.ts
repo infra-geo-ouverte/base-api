5c7b4dd7b869e1fc71e7a293d16fb9c489619510
Philippe Lafreniere <philippe.lafreniere@msp.gouv.qc.ca>
2022-03-15 10:16:00 -0400

    fix(sanitizer): fix readabled array stringify

--- src/plugins/sanitizer/sanitizer.ts
+++ src/plugins/sanitizer/sanitizer.ts
@@ -5,8 +5,12 @@ export class Sanitizer {
   static sanitize(obj: any) {
     if (typeof obj === 'object') {
       for (const value in obj) {
-        if (!(obj[value] instanceof Readable)) {
-          obj[value] = JSON.parse(filterXSS(JSON.stringify(obj[value])));
+        if (obj[value] instanceof Array) {
+          for (const object of obj[value]) {
+            obj[object] = Sanitizer.sanitize(obj[object]);
+          }
+        } else if (!(obj[value] instanceof Readable)) {
+            obj[value] = JSON.parse(filterXSS(JSON.stringify(obj[value])));
         }
       }
     } else if (typeof obj === 'string') {
