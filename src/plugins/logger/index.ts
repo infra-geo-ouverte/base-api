b3f91703bc08ba4a621fe2c9b79c395b0f9da3f2
Philippe Lafreniere <philippe.lafreniere@msp.gouv.qc.ca>
2022-03-15 11:28:54 -0400

    fix: ignore function

--- src/plugins/logger/index.ts
+++ src/plugins/logger/index.ts
@@ -34,7 +34,7 @@ export default (): IPlugin => {
           logRequestStart: false,
           ignorePaths: [],
           ignoreTags: [],
-          ignoreFunc: (request) => {
+          ignoreFunc: (_ignoreOptions, request) => {
             const ip = request.headers ? request.headers['x-real-ip'] : undefined;
             return loggerOptions.exclude && loggerOptions.exclude.ips && loggerOptions.exclude.ips.includes(ip);
           }
