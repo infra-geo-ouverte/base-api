fad896a5ab91a6fdec4f356c24dc9d2a87e34c17
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2023-10-12 14:32:42 -0400

    fix(cache): fix cache after update

--- src/core/server.ts
+++ src/core/server.ts
@@ -50,7 +50,7 @@ export class Server {
     if (configs.cache && configs.cache.engine === 'redis') {
       redisCacheConfig = {
         provider: {
-          constructor: require('@hapi/catbox-redis'),
+          constructor: require('@hapi/catbox-redis').Engine,
           options: {
             host: configs.cache.host,
             partition: configs.cache.partition
