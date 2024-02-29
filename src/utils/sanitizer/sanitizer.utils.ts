9f0d6c357c90e81217c2ba9e1d257e35783d4fcf
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2024-02-29 13:26:26 -0500

    feat: sanitizer handle regex

--- src/utils/sanitizer/sanitizer.utils.ts
+++ src/utils/sanitizer/sanitizer.utils.ts
@@ -33,4 +33,15 @@ export class Sanitizer {
 
     return stringSanitized;
   }
+
+  // Source: https://github.com/sindresorhus/escape-string-regexp
+  static escapeStringRegex(value: string): string {
+    if (typeof value !== 'string') {
+      throw new TypeError('Expected a string');
+    }
+
+    // Escape characters with special meaning either inside or outside character sets.
+    // Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
+    return value.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
+  }
 }
